3f0c54e0d94c4f14099110cb07ae45d1b335c7b7
girikalra <girikalra@gmail.com>
2018-02-09 16:39:11 -0800

    Working schema

--- server/schema.sql
+++ server/schema.sql
@@ -1,10 +1,22 @@
+DROP DATABASE chat;
 CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
+CREATE TABLE rooms (
+  id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name varchar(255) NOT NULL
 );
+
+CREATE TABLE messages (
+  msgID int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  username varchar(15) NOT NULL,
+  message varchar(255) NOT NULL,
+  roomnameID int NOT NULL,
+  FOREIGN KEY (roomnameID) REFERENCES rooms(id)
+);
+
+
 
 /* Create other tables and define schemas for them here! */
 
